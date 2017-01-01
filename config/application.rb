dc608914ef3d4539105f18830f38c175ac98d9f7
El R <jrhee17@coupang.com>
2017-01-01 23:21:53 +0900

    mongoid config 2

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ Bundler.require(*Rails.groups)
 
 module Exaka
   class Application < Rails::Application
-      Mongoid.load!('./config/mongoid.yml')
+      Mongoid.load!(File.expand_path('mongoid.yml', './config'))
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
